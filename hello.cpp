104ba951f85037ca1fcbd7872c9bf21fafe41407
Abdul Manan Shah <abdulmananshah02@gmail.com>
2023-09-19 10:05:56 +0500

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,10 +4,5 @@ int main()
 {
     cout << "HELLO WORLD";
     cout << "CHANGES TO FETCH WITH GIT PULL";
-<<<<<<< HEAD
-    cout << "CHANGES TO RESOLVE CONFLICT";
-=======
-    cout << "CONFLICT";
->>>>>>> branch1
     cout << "CHANGES TO RESOLVE CONFLICT";
 }
